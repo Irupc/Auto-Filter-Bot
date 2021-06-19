b372cc7a6a03d2a10cbf3d5e923ebce938601614
Irupc <68991705+Irupc@users.noreply.github.com>
2021-06-19 16:08:15 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -34,7 +34,7 @@ async def filter(client: Bot, message: Message):
         async for msg in client.USER.search_messages(MAINCHANNEL_ID,query=message.text,filter='document'):
             file_name = msg.document.file_name
             msg_id = msg.message_id                     
-            link = msg.link
+            link = "https://irupc.net/"+msg.link.split("/")[4]
             btn.append(
                 [InlineKeyboardButton(text=f"{file_name}",url=f"{link}")]
             )
